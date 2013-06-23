74b831259dd740fec9a57c10655e7f6189a60358
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2013-06-23 12:09:30 -0700

    Added function for finding the index streamer file

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -1,7 +1,18 @@
 defmodule Streamers do
 
-  def find_index do
-
+  @@@doc """
+  Find streaming file in the given directory.
+  """
+  def find_index(directory) do
+    files = Path.join(directory, "*.m3u8")
+    if file = Enum.find(Path.wildcard(files), is_index?(&1)) do
+      Path.basename file
+    end
   end
 
+  defp is_index?(file) do
+    File.open! file, fn(pid) ->
+      IO.read(pid, 25)  == "#EXTM3U\n#EXT-X-STREAM-INF"
+    end
+  end
 end
