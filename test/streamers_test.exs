f86cd21bf9ee624d50fd876e90061f08722db34d
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2013-06-23 17:01:33 -0700

    Extra test added

--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -7,4 +7,8 @@ defmodule StreamersTest do
     assert Streamers.find_index('test/fixtures/emberjs') ==
       "9af0270acb795f9dcafb5c51b1907628.m3u8"
   end
+
+  test "returns nil for not available index files" do
+    assert Streamers.find_index('this/doesnt/exist') == nil
+  end
 end
