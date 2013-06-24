e6ef8db024dcd385405137c47ea7047baa5866e6
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2013-06-23 21:41:19 -0700

    added parallelization

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -1,6 +1,6 @@
 defmodule Streamers do
-  defrecord M3U8, program_id: nil, path: nil, bandwidth: nil
-  @@@doc """
+  defrecord M3U8, program_id: nil, path: nil, bandwidth: nil, ts_files: []
+  @doc """
   Find streaming file in the given directory.
 
   ## Examples
@@ -15,6 +15,15 @@ defmodule Streamers do
     end
   end
 
+  defp is_index?(file) do
+    File.open! file, fn(pid) ->
+      IO.read(pid, 25)  == "#EXTM3U\n#EXT-X-STREAM-INF"
+    end
+  end
+
+  @doc """
+  Extracts M3U8 records from the index file
+  """
   def extract_m3u8(index_file) do
     File.open! index_file, fn(pid) ->
       IO.readline(pid)
@@ -32,7 +41,6 @@ defmodule Streamers do
   end
 
   defp do_extract_m3u8(pid, dir, stream_inf, path, acc) do
-    # #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=110000
     path = Path.join(dir, path |> String.strip)
     << "#EXT-X-STREAM-INF:PROGRAM-ID=", program_id, ",BANDWIDTH=", bandwidth :: binary  >> = stream_inf
     prog_id = binary_to_integer <<program_id>>
@@ -40,9 +48,50 @@ defmodule Streamers do
     do_extract_m3u8(pid, dir, [record|acc])
   end
 
-  defp is_index?(file) do
-    File.open! file, fn(pid) ->
-      IO.read(pid, 25)  == "#EXTM3U\n#EXT-X-STREAM-INF"
+  @doc """
+  Process M3U8 records to get ts_files
+  """
+
+  def process_m3u8(m3u8s) do
+    Enum.map m3u8s, do_parallel_process_m3u8(&1, self)
+    do_collect_m3u8(length(m3u8s), [])
+  end
+
+  defp do_collect_m3u8(0, acc), do: acc
+
+  defp do_collect_m3u8(count, acc) do
+    receive do
+      {:m3u8, updated_m3u8} ->
+        do_collect_m3u8(count-1, [updated_m3u8|acc])
+
     end
+  end
+
+  defp do_parallel_process_m3u8(m3u8, parent_pid) do
+    spawn (fn ->
+      updated_m3u8 = do_process_m3u8(m3u8)
+
+      parent_pid <- {:m3u8, updated_m3u8}
+
+    end)
+  end
+
+  defp do_process_m3u8(M3U8[path: path] = m3u8) do
+    File.open! path, fn(pid) ->
+      IO.readline(pid)
+      IO.readline(pid)
+
+      m3u8.ts_files(do_process_m3u8(pid, []))
+    end
+  end
+
+  defp do_process_m3u8(pid, acc) do
+    case IO.readline(pid) do
+      "#EXT-X-ENDLIST\n" -> Enum.reverse(acc)
+      #discards #Extinf:10
+      extinf when is_binary(extinf)->
+        file = IO.readline(pid) |> String.strip
+        do_process_m3u8(pid,[file|acc])
+      end
   end
 end
--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -1,10 +1,11 @@
 Code.require_file "test_helper.exs", __DIR__
 
 defmodule StreamersTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   alias Streamers.M3U8 , as: M3U8
   doctest Streamers
   @index_file "test/fixtures/emberjs/9af0270acb795f9dcafb5c51b1907628.m3u8"
+  @m3u8_sample "test/fixtures/emberjs/8bda35243c7c0a7fc69ebe1383c6464c.m3u8"
 
   test 'finds index file in a directory' do
     assert Streamers.find_index("test/fixtures/emberjs") == @index_file
@@ -17,13 +18,17 @@ defmodule StreamersTest do
   test "extracts m3u8" do
     m3u8s =  Streamers.extract_m3u8(@index_file)
     assert Enum.first(m3u8s) ==
-      M3U8[program_id: 1, bandwidth: 110000, path: "test/fixtures/emberjs/8bda35243c7c0a7fc69ebe1383c6464c.m3u8"]
+      M3U8[program_id: 1, bandwidth: 110000, path: @m3u8_sample]
 
     assert length(m3u8s) == 5
     # PROGRAM-ID=1,BANDWIDTH=110000
     # 8bda35243c7c0a7fc69ebe1383c6464c.m3u8
-
   end
 
 
+  test "processes m3u8" do
+    m3u8s = @index_file |> Streamers.extract_m3u8 |> Streamers.process_m3u8
+    sample = Enum.find(m3u8s, (fn(m3u8)-> m3u8.path == @m3u8_sample end))
+    assert length(sample.ts_files) == 510
+  end
 end
