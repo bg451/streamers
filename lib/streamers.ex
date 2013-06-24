bd5a9cbe70ff1273e05ebdbe41d4ba6dffff0258
Brandon Gonzalez <brandon.gonzalez.451@gmail.com>
2013-06-23 19:03:11 -0700

    Added extract_m3u8 however it needs to work

--- lib/streamers.ex
+++ lib/streamers.ex
@@ -1,12 +1,17 @@
 defmodule Streamers do
-
+  defrecord M3U8, program_id: nil, path: nil, bandwidth: nil
   @@@doc """
   Find streaming file in the given directory.
+
+  ## Examples
+
+    iex> Streamers.find_index("this/doesnt/exist")
+      nil
   """
   def find_index(directory) do
     files = Path.join(directory, "*.m3u8")
     if file = Enum.find(Path.wildcard(files), is_index?(&1)) do
-      Path.basename file
+       file
     end
   end
 
@@ -14,5 +19,30 @@ defmodule Streamers do
     File.open! file, fn(pid) ->
       IO.read(pid, 25)  == "#EXTM3U\n#EXT-X-STREAM-INF"
     end
+  end
+
+  def extract_m3u8(index_file) do
+    File.open! index_file, fn(pid) ->
+      IO.readline(pid)
+      do_extract_m3u8(pid, [])
+    end
+  end
+
+  defp do_extract_m3u8(pid, acc) do
+    case IO.readline(pid) do
+      :eof -> Enum.reverse(acc)
+      stream_inf ->
+        path = IO.readline(pid)
+        do_extract_m3u8(pid, stream_inf, path, acc)
+    end
+  end
+
+  defp do_extract_m3u8(pid, stream_inf, path, acc) do
+    # #EXT-X-STREAM-INF:PROGRAM-ID=1,BANDWIDTH=110000
+
+    << "#EXT-X-STREAM-INF:PROGRAM-ID=", program_id, ",BANDWIDTH=", bandwidth :: binary  >> = stream_inf
+    prog_id = binary_to_integer <<program_id>>
+    record = M3U8[program_id: prog_id, path: String.strip(path), bandwidth: String.strip bandwidth]
+    do_extract_m3u8(pid, [record|acc])
   end
 end
--- test/streamers_test.exs
+++ test/streamers_test.exs
@@ -2,13 +2,24 @@ Code.require_file "test_helper.exs", __DIR__
 
 defmodule StreamersTest do
   use ExUnit.Case
+  alias Streamers.M3U8 , as: M3U8
+  doctest Streamers
+  @index_file "test/fixtures/emberjs/9af0270acb795f9dcafb5c51b1907628.m3u8"
 
-  test 'find index file in a directory' do
-    assert Streamers.find_index('test/fixtures/emberjs') ==
-      "9af0270acb795f9dcafb5c51b1907628.m3u8"
+  test 'finds index file in a directory' do
+    assert Streamers.find_index("test/fixtures/emberjs") == @index_file
   end
 
   test "returns nil for not available index files" do
     assert Streamers.find_index('this/doesnt/exist') == nil
+  end
+
+  test "extracts m3u8" do
+    m3u8s =  Streamers.extract_m3u8(@index_file)
+    assert Enum.first(m3u8s) ==
+    M3U8[program_id: 1, bandwidth: 110000, path: "test/fixtures/emberjs/8bda35243c7c0a7fc69ebe1383c6464c.m3u8"]
+
+    # PROGRAM-ID=1,BANDWIDTH=110000
+    # 8bda35243c7c0a7fc69ebe1383c6464c.m3u8
   end
 end
